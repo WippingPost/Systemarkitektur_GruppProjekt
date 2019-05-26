16f836f175db24d9c6b1fe8fcae2971c33e0a50b
Anders <anders70nilsson@gmail.com>
2019-05-26 18:12:03 +0200

    Mer kod

--- src/rPiController/RaspberryPi.java
+++ src/rPiController/RaspberryPi.java
@@ -1,8 +1,26 @@
 package rPiController;
 
+
 public class RaspberryPi extends RelayBoard {
 
 	public RaspberryPi() {
 		// TODO ......
+	}
+
+
+	public void turnOnLights() {	// 5V signal till relä
+		relayLights.activate();
+	}
+
+	public void turnOnCoffeeMaker() {	// 5V signal till relä
+		relayCoffeeMaker.activate();
+	}
+
+	public void turnOnMediaPlayer() {	// 5V signal till relä
+		relayMediaPlayer.activate();
+	}
+
+	public void turnOnWelcomeScreen() {	// 5V signal till relä
+		relayWelcomeScreen.activate();
 	}
 }
--- src/rPiController/RelayBoard.java
+++ src/rPiController/RelayBoard.java
@@ -5,4 +5,5 @@ public class RelayBoard {
 	Relay relayLights = new Relay(false);
 	Relay relayMediaPlayer = new Relay(false);
 	Relay relayWelcomeScreen = new Relay(false);
+	Relay relayCoffeeMaker = new Relay(false);
 }
